448f3a8fd6b492aa19fc2a1fec603de50ec9e854
blackpanzero <derrickotieno41@gmail.com>
2020-11-11 15:02:55 +0300

    init

--- Boutique55/settings.py
+++ Boutique55/settings.py
@@ -139,6 +139,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-
 # Activate Django-Heroku.
 django_heroku.settings(locals())

06c63b031d16047ae54d5a1682ad1eaa27b26ce5
blackpanzero <derrickotieno41@gmail.com>
2020-11-12 14:45:33 +0300

    init

--- Boutique55/settings.py
+++ Boutique55/settings.py
@@ -140,7 +140,7 @@ EMAIL_USE_SSL = False
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
